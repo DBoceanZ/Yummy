42ea1656486d8c2a38b7bdceccf348184aceeef6
nhasty <nyall.hasty@gmail.com>
2022-11-23 16:54:51 -0800

    fixed file names

--- server/controllers/getVideoData.js
+++
@@ -1,8 +0,0 @@
-const selectVideoData = require('../model/selectVideoData');
-
-module.exports = {
-  videoComments: async (req, res) => {
-    const allComments = await selectVideoData.selectComments(req.query.video_id)
-    res.status(200).json(allComments)
-  }
-}
