9c91b1ffafd3e66157f4b85972d6431be4e96a9c
nhasty <nyall.hasty@gmail.com>
2022-11-25 18:02:20 -0800

    videos/home endpoint gets videos for user based on interests

--- server/routes/tags.js
+++
@@ -1,6 +0,0 @@
-const tagsRouter = require('express').Router();
-const tagController = require('../controllers/tagController');
-
-tagsRouter.get('/tags', tagController.getTags);
-
-module.exports = tagsRouter
