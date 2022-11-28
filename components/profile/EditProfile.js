1c18085298a62c4e11102fbbcb5b9758cf115955
Cole Bienek <colebienek@gmail.com>
2022-11-28 07:45:25 -0800

    color match headers

--- components/profile/EditProfile.js
+++ components/profile/EditProfile.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
 import EditProfileHeader from './EditProfileHeader.js';
+import { StatusBar } from 'expo-status-bar';
 
 const EditProfile = () => {
   return (
     <View>
+      <StatusBar barStyle="dark-content" translucent={false} />
       <EditProfileHeader />
     </View>
-  )
+  );
 };
 
 export default EditProfile;
