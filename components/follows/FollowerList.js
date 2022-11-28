b4e805569ea118f443c7fcd51d09d5fad049f80f
Robert Garcia <robertvincentgarciasr@gmail.com>
2022-11-27 22:07:27 -0800

    add page focus to follows components

--- components/follows/FollowerList.js
+++ components/follows/FollowerList.js
@@ -9,22 +9,26 @@ export default function Followers({ navigation }) {
   const { userData, setUserData } = useGlobalContext();
   const { selectedUserID } = userData;
   const [followerList, setFollowerList] = useState([]);
-  const test_id = 1;
-
-  useEffect(() => {
-    axios.get(`http://18.212.89.94:3000/follows/followers?user_followed_id=3`)
-      .then(results => {
-        setFollowerList(results.data)
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [])
 
   const handleProfileNavigation = (id) => {
     setUserData({ ...userData, selectedUserID: id });
     navigation.navigate('Profile');
   }
+
+  useEffect(() => {
+
+    const unsubscribe = navigation.addListener('focus', () => {
+      axios.get(`http://18.212.89.94:3000/follows/followers?user_followed_id=3`)
+        .then(results => {
+          setFollowerList(results.data)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+    return unsubscribe;
+  }, [navigation])
+
 
   const renderItem = ({ item }) => (
     <ListItem user={item} buttonName={"Remove"} handleProfileNavigation={handleProfileNavigation} />
