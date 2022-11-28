fe2d56609f6763543f0f53661d6c4fd7be790222
jake-manning <jcmanning92@gmail.com>
2022-11-27 23:33:44 -0800

    Refactor Profile to use UID

--- components/profile/ProfileHeader.js
+++ components/profile/ProfileHeader.js
@@ -7,46 +7,19 @@ import axios from 'axios';
 
 const ProfileHeader = ({ handlers, navigation }) => {
   const { userData } = useGlobalContext();
-  const { UID, selectedUserID } = userData;
+  const { UID } = userData;
   const { 
     handleFollowersTouch, handleFollowingTouch, handleEditProfileTouch 
   } = handlers;
   const [username, setUsername] = useState('ExampleUser');
   const [profilePhoto, setProfilePhoto] = useState('http://tinyurl.com/68dvbhaw');
-  const [bio, setBio] = useState(
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-  );
+  const [bio, setBio] = useState('');
   const [likes, setLikes] = useState(0);
-  const [following, setFollowing] = useState(undefined);
-
-  // follow button press handlers
-  const handleFollowPress = () => {
-    axios.post('http://18.212.89.94:3000/users/follow', {
-      user_id: UID,
-      followed_id: selectedUserID
-    })
-      .then(() => {
-        setFollowing(true);
-      })
-      .catch((err) => console.log(err));
-  };
-
-  const handleUnfollowPress = () => {
-    axios.delete('http://18.212.89.94:3000/users/follow', {
-      data: {
-        user_id: UID,
-        followed_id: selectedUserID
-      }})
-      .then(() => {
-        setFollowing(false);
-      })
-      .catch((err) => console.log(err));
-  };
 
   // on page focus, fetch user profile data from database
   useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
-      axios.get(`http://18.212.89.94:3000/users/userData?user_id=${selectedUserID}`)
+      axios.get(`http://18.212.89.94:3000/users/userData?user_id=${UID}`)
       .then((res) => {
         setUsername(res.data.username);
         setBio(res.data.bio);
@@ -63,23 +36,6 @@ const ProfileHeader = ({ handlers, navigation }) => {
         }
       })
       .catch((err) => console.log(err));
-    });
-    return unsubscribe;
-  }, [navigation]);
-
-  useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
-      axios.get(`http://18.212.89.94:3000/follows/following?user_following_id=${UID}`)
-        .then((res) => {
-          let isFollowing = false;
-          res.data.forEach((user) => {
-            if (user.id == selectedUserID) {
-              isFollowing = true;
-            }
-          })
-          setFollowing(isFollowing);
-        })
-        .catch((err) => console.log(err));
     });
     return unsubscribe;
   }, [navigation]);
@@ -114,15 +70,7 @@ const ProfileHeader = ({ handlers, navigation }) => {
         <Text style={styles.bio}>{bio}</Text>
       </View>
       <View style={styles.buttonContainer}>
-        {
-          UID === selectedUserID ? 
-            <LightButton onPress={() => handleEditProfileTouch()} text='Edit Profile'/> :
-            following === undefined ? 
-              <></> :
-              following === false ?
-                <LightButton onPress={() => handleFollowPress()} text='Follow'/> :
-                <LightButton onPress={() => handleUnfollowPress()} text='Unfollow'/>
-        }
+        <LightButton onPress={() => handleEditProfileTouch()} text='Edit Profile'/> :
       </View>
     </View>
   )
